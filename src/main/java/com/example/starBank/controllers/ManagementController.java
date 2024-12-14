cdfcd40feb5a4ff2daf3b24782c3ef3e00aeb3bb
Юрий <fedejkin@gmail.com>
2024-12-14 08:54:24 +0300

    Добалены классы ManagementController и PomXmlParser для получения данных проекта

--- src/main/java/com/example/starBank/controllers/ManagementController.java
+++ src/main/java/com/example/starBank/controllers/ManagementController.java
@@ -1,13 +1,24 @@
 package com.example.starBank.controllers;
 
+import com.example.starBank.services.PomXmlParser;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 
 @RequestMapping("/management")
 @RestController
 public class ManagementController {
+
+    PomXmlParser pomXmlParser;
+
+    public ManagementController(PomXmlParser pomXmlParser) {
+        this.pomXmlParser = pomXmlParser;
+    }
 
     /**
      * Метод очищения всех закешированных результатов по имени кеша
@@ -15,4 +26,18 @@ public class ManagementController {
     @PostMapping("/clear-caches")
     @CacheEvict(cacheNames = "Recommendations", allEntries = true)
     public void clearCache() {}
+
+    /**
+     * Метод получения имени и версии проекта из файла pom.xml
+     * @throws IOException Ошибки ввода-вывода
+     * @throws ParserConfigurationException Ошибки в конфигурации
+     * @throws SAXException  Ошибки формата и содержания входящих или исходящих XML-данных
+     * @return Возвращает строку с именем и версией проекта
+     */
+    @PostMapping("/info")
+    public String getInfo() throws IOException, ParserConfigurationException, SAXException {
+        String str = null;
+        str = pomXmlParser.getData();
+        return str;
+    }
 }
